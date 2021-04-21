0e0514a57d35298f93b4c52b4ba05da8c59bb4ea
Sanjeevlu Buggargani <35345738+nsanju0413@users.noreply.github.com>
2021-04-21 12:24:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,19 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+          <marquee><h1>Stonedge Technologies</h1></marquee>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
